0f2d54a8352e6bcecfef9d59c276781e4a1d02ac
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-03 01:20:39 +0500

    update

--- index6.js
+++ index6.js
@@ -8,11 +8,12 @@ var c = a + b;
 document.write("<h2>Sum of " + a + " " + "and" + " " + b + " " + "is" + " " + c +" <h2>");
 
 var d = a-b;
-document.write("<h2>Subtration of " + a + " " + "and" + " "+ b + " " + "is" + " " + d + " </h2>"); 
+document.write("<h2>Subtration from " + a + " " + "to" + " "+ b + " " + "is" + " " + d + " </h2>"); 
 
 var e = a * b;
 document.write("<h2> Product of " + a + " " + "and" + b + " " + "is" + " " + e + " </<h2>");
 var f = a / b;
+
 var g = a%b;
-document.write("<h2> Division of " +a + " " + "and" + b+ " "+ "is" + " " + f + " </h2>");
+document.write("<h2> Division from " +a + " " + "to" + b+ " "+ "is" + " " + f + " </h2>");
 document.write("<h2> Modulus of " +a + " " + "and" + b + " " + "is" + g +  " <h2>");   
