482d0ea8fdc8719f4cc180987efb85dd8d7e012e
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-03 00:58:38 +0500

    update

---
+++ index6.js
@@ -0,0 +1,18 @@
+alert("Assignment 1 Task 8");
+var a = prompt("Enter 1st number");
+var b = prompt("Enter 2nd number");
+a = parseFloat(a);
+b = parseFloat(b);
+var c = a + b;
+
+document.write("<h2>Sum of " + a + " " + "and" + " " + b + " " + "is" + " " + c +" <h2>");
+
+var d = a-b;
+document.write("<h2>Subtration of " + a + " " + "and" + " "+ b + " " + "is" + " " + d + " </h2>"); 
+
+var e = a * b;
+document.write("<h2> Product of " + a + " " + "and" + b + " " + "is" + " " + e + " </<h2>");
+var f = a / b;
+var g = a%b;
+document.write("<h2> Division of " +a + " " + "and" + b+ " "+ "is" + " " + f + " </h2>");
+document.write("<h2> Modulus of " +a + " " + "and" + b + " " + "is" + g +  " <h2>");   
