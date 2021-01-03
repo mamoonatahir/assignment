9e8054f716d0271d745ce0ea07dbe2053879337f
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-03 15:32:27 +0500

    rename

--- task11.js
+++ task11.js
@@ -2,10 +2,11 @@
 document.write("<h2> Assignment 1 Task 11</h2>");
 var tempC = 44;
 var F = (tempC *9/5) +32;
-document.write("<h3> <br>  " + tempC + "is " + " "+ F + "F");
+document.write("<h3> <br>  " + tempC + " °C  is " + " "+ F +"°F");
 var tempF = 95;
 var C = (tempF -32) * 5/9;
-document.write("<br> " + tempF+ "F  is" + " "+ C + " C");
+document.write("<br> " + tempF+ " °F  is" + " "+ C + "°C ");
+
 
 
 
