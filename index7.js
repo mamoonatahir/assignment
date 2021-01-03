ecaf4a763d857cf646496214acc9c9fa11149afa
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-03 10:14:19 +0500

    update

--- index7.js
+++ index7.js
@@ -1,3 +1,17 @@
 var a;
-document.write("Value after variable declaration is" + " " +a);
-console.log(a);
+document.write("<h3>Value after variable declaration is" + " " +a);
+a = 5;
+document.write(" <br> Initial value : " + " "+ a);
+a++;
+document.write("<br> value after increment is : " + " "+ a);
+b = a + 7;
+document.write("<br> Value after addition is: " + ' '+ b);
+b--;
+document.write(" <br> value after decrement is :" + " " + b );
+c = b%3;
+document.write("<br> The reminder is :  " + " " + c); 
+
+
+
+
+
