f8c0be59eef375e9b9b3a7d79ef70938d46727ec
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-03 11:34:31 +0500

    update

--- index7.js
+++ index7.js
@@ -1,15 +1,16 @@
+document.write("<h2>Assignment 1 Task 10 </h2>");
 var a;
 document.write("<h3>Value after variable declaration is" + " " +a);
-a = 5;
+a = 9;
 document.write(" <br> Initial value : " + " "+ a);
 a++;
 document.write("<br> value after increment is : " + " "+ a);
-b = a + 7;
+b = a + 5;
 document.write("<br> Value after addition is: " + ' '+ b);
 b--;
 document.write(" <br> value after decrement is :" + " " + b );
-c = b%3;
-document.write("<br> The reminder is :  " + " " + c); 
+c = b%7;
+document.write("<br> The reminder is :  " + " " + c + " </h3>"); 
 
 
 
