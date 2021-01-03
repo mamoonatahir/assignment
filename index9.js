92786b69158cca396e8c46e052154105df67addc
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-03 11:34:01 +0500

    update

---
+++ index9.js
@@ -0,0 +1,13 @@
+
+document.write("<h2> Assignment 1 Task 11</h2>");
+var tempC = 44;
+var F = (tempC *9/5) +32;
+document.write("<h3> <br>  " + tempC + "is " + " "+ F + "F");
+var tempF = 95;
+var C = (tempF -32) * 5/9;
+document.write("<br> " + tempF+ "F  is" + " "+ C + " C");
+
+
+
+
+ 
