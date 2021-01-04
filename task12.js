ec54d4879b72bdc2c416f49c8e9fcd784f0f109f
mamoonatahir <mamoona.tahir3@gmail.com>
2021-01-04 07:00:51 +0500

    update

---
+++ task12.js
@@ -0,0 +1,5 @@
+document.write("<h1> Currency in PKR </h1>");
+var us = 155;
+var sr = 41;
+var pkr = us*10 + sr*25;
+document.write("<h3> Total Currency in PKR: " + " " + pkr);
---
+++ task13.js
@@ -0,0 +1,6 @@
+var a = 10;
+document.write("<h1> Result:</h1>");
+document.write("<h3> The value of a is : " + a );
+
+
+
